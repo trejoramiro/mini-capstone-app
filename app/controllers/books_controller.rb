c5ec6c6c965b104c557e0e3fd9f8198770e85002
Ramiro Trejo <trejoramiro1@gmail.com>
2016-07-11 21:30:56 -0500

    Finalized CRUD function.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,61 @@
 class BooksController < ApplicationController
 
-  def get_all_books
+  def index
     @books = Book.all
-    render 'all_books.html.erb'
+    render 'index.html.erb'
   end
 
-  def get_one_book
+  def show
+    # using the hash "params[]"
     @book = Book.find_by(id: params[:id])
-    render 'book.html.erb'
+    render 'show.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @new_book = Book.new({
+      title: params[:form_book_title],
+      author: params[:form_author],
+      price: params[:form_price],
+      publisher: params[:form_publisher],
+      image: params[:form_image],
+      date_published: params[:form_date_published],
+      genre: params[:form_genre]
+      })
+
+    @new_book.save
+
+    render 'create.html.erb'
+  end
+
+  def edit
+    @book = Book.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+    #updates and saves at the same time
+    @book.update(
+      title: params[:form_book_title],
+      author: params[:form_author],
+      price: params[:form_price],
+      publisher: params[:form_publisher],
+      image: params[:form_image],
+      genre: params[:form_genre],
+      date_published: params[:form_date_published]
+      )
+
+    render 'update.html.erb'
+  end
+
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    @book.destroy
+    render 'destroy.html.erb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 
-  get '/books' => 'books#get_all_books'
+  get 'books' => 'books#index'
 
-  get '/book/:id' => 'books#get_one_book'
+  get 'books/new' => 'books#new'
+
+  post 'books' => 'books#create'
+
+  get 'books/:id' => 'books#show'
+
+  get 'books/:id/edit' => 'books#edit'
+
+  patch 'books/:id' => 'books#update'
+
+  delete 'books/:id' => 'books#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
