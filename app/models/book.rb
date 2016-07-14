d28c173f14091697b89538d0fc32b017190a932c
Ramiro Trejo <trejoramiro1@gmail.com>
2016-07-14 17:48:12 -0500

    Styles added.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,30 @@
 class Book < ActiveRecord::Base
+  #great place to add logic  about your model
+  def sales_message
+    sale_price = 10
+    price_array = price.split('.')
+    # price is a method built by Rails that gets the price value in the database and returns it
+    # don't overwrite the getter Rails
+    if price_array[0].to_i <= sale_price
+      message = true
+    else
+      message = false
+    end
+    return message
+  end
+
+  def tax
+    tax = 0.09
+    return tax
+  end
+
+  def total
+    total = price.to_f + (price.to_f * tax)
+    return total
+  end
+
+  def formatted_date
+    date_published
+  end
+
 end
